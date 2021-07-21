033377a1d11f470992bf2e578133d19c32d31063
Emily Pozzi <epozzi5@yahoo.com>
2021-07-21 00:09:36 -0500

    generate html?

--- __tests__/Managers_test.js
+++ __tests__/Managers_test.js
@@ -1,16 +1,13 @@
-// using Manager constructor 
 const Manager = require('../lib/Manager');
-
-// creating manager object  
+ 
 test('creates an Manager object', () => {
-    const manager = new Manager('Nicole', 90, 'nicole.elisaw@gmail', 4);
+    const manager = new Manager('Jenny', 24, 'jenny@mail.com', 5);
 
     expect(manager.officeNumber) .toEqual(expect.any(Number));
 });
 
-// gets role from getRole()
 test('gets role of employee', () => {
-    const manager = new Manager('Nicole', 90, 'nicole.elisaw@gmail.com');
+    const manager = new Manager('Jenny', 24, 'jenny@mail.com');
 
     expect(manager.getRole()).toEqual("Manager");
 });  
